475cd5d9989fda0c4489fc39708cc50ef2b52fe8
khan-shavez <shavezkhanit@gmail.com>
2025-08-09 19:47:08 +0530

    Updated global layout structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,36 @@
-import { Stack } from "expo-router";
+/* eslint-disable import/no-named-as-default */
+import GlobalProvider from "@/lib/global-provider";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import './global.css';
-
+ 
+// SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
-  return <Stack />;
+
+  const [fontsLoaded] = useFonts({
+    "Rubik-Bold": require('../assets/fonts/Rubik-Bold.ttf'),
+    "Rubik-ExtraBold": require('../assets/fonts/Rubik-ExtraBold.ttf'),
+    "Rubik-Light": require('../assets/fonts/Rubik-Light.ttf'),
+    "Rubik-Medium": require('../assets/fonts/Rubik-Medium.ttf'),
+    "Rubik-Regular": require('../assets/fonts/Rubik-Regular.ttf'),
+    "Rubik-SemiBold": require('../assets/fonts/Rubik-SemiBold.ttf'),
+  })
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  },[fontsLoaded])
+
+  if(!fontsLoaded) return null
+  
+  return (
+    <GlobalProvider>
+       <Stack screenOptions={{headerShown: false}} />;
+    </GlobalProvider>
+   
+  )
+  
+  
 }
